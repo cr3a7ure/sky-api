0df4fe5c418161dfd6f08d82c63c7739320195df
Dimitris <grmitsos@gmail.com>
2017-10-01 02:18:51 +0300

    fixed date format

--- src/AppBundle/DataProvider/OfferCollectionDataProvider.php
+++ src/AppBundle/DataProvider/OfferCollectionDataProvider.php
@@ -191,6 +191,18 @@ final class OfferCollectionDataProvider implements CollectionDataProviderInterfa
             }
 
         } else {
+            if (array_key_exists('itemOffered_arrivalTime',$props)) {
+                $da = \DateTime::createFromFormat(\DateTime::W3C,$props['itemOffered_arrivalTime']);
+                $inboundPartialDate =  $da->format('Y-m');
+            } else {
+                $inboundPartialDate = '2017-12';
+            }
+            if (array_key_exists('itemOffered_departureTime',$props)) {
+                $dd = \DateTime::createFromFormat(\DateTime::W3C,$props['itemOffered_departureTime']);
+                $outboundPartialDate =  $dd->format('Y-m');
+            } else {
+                $outboundPartialDate = '2017-12';
+            }
             $query = array();
             $query['apikey'] = $skyscannerKey;
             $marketCountry = 'GR';
